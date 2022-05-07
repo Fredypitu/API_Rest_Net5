edf2fb1decbeaf12fc19731f19c06d9faaaa918f
Duarte <alfredo.f.duarte@accenture.com>
2022-05-07 00:34:13 -0300

    Orden y Producto Controller funcionando y todos los archivos de configuracion en JMusik.data realizados

--- JMusik.Data/TiendaDbContext.cs
+++ JMusik.Data/TiendaDbContext.cs
@@ -38,67 +38,15 @@ namespace JMusik.Data
         {
             modelBuilder.HasAnnotation("Relational:Collation", "SQL_Latin1_General_CP1_CI_AS");
 
-            modelBuilder.Entity<DetalleOrden>(entity =>
-            {
-                entity.ToTable("DetalleOrden", "tienda");
 
-                entity.HasIndex(e => e.OrdenId, "IX_DetalleOrden_OrdenId");
-
-                entity.HasIndex(e => e.ProductoId, "IX_DetalleOrden_ProductoId");
-
-                entity.Property(e => e.Cantidad).HasColumnType("decimal(18, 2)");
-
-                entity.Property(e => e.PrecioUnitario).HasColumnType("decimal(18, 2)");
-
-                entity.Property(e => e.Total).HasColumnType("decimal(18, 2)");
-
-                entity.HasOne(d => d.Orden)
-                    .WithMany(p => p.DetalleOrdens)
-                    .HasForeignKey(d => d.OrdenId);
-
-                entity.HasOne(d => d.Producto)
-                    .WithMany(p => p.DetallesOrden)
-                    .HasForeignKey(d => d.ProductoId);
-            });
 
             modelBuilder.ApplyConfiguration(new OrdenConfiguracion());
-       
-            modelBuilder.Entity<Perfil>(entity =>
-            {
-                entity.ToTable("Perfil", "tienda");
+            modelBuilder.ApplyConfiguration(new ProductoConfiguracion());
+            modelBuilder.ApplyConfiguration(new DetalleOrdenConfiguracion());
+            modelBuilder.ApplyConfiguration(new PerfilConfiguracion());
+            modelBuilder.ApplyConfiguration(new UsuarioConfiguracion());
 
-                entity.Property(e => e.Nombre).HasMaxLength(50);
-            });
 
-            modelBuilder.Entity<JMusik.Models.Producto>(entity =>
-            {
-                entity.ToTable("Producto", "tienda");
-
-                entity.Property(e => e.Nombre).HasMaxLength(256);
-
-                entity.Property(e => e.Precio).HasColumnType("decimal(18, 2)");
-            });
-
-            modelBuilder.Entity<Usuario>(entity =>
-            {
-                entity.ToTable("Usuario", "tienda");
-
-                entity.HasIndex(e => e.PerfilId, "IX_Usuario_PerfilId");
-
-                entity.Property(e => e.Apellidos).HasMaxLength(256);
-
-                entity.Property(e => e.Email).HasMaxLength(100);
-
-                entity.Property(e => e.Nombre).HasMaxLength(50);
-
-                entity.Property(e => e.Password).HasMaxLength(512);
-
-                entity.Property(e => e.Username).HasMaxLength(25);
-
-                entity.HasOne(d => d.Perfil)
-                    .WithMany(p => p.Usuarios)
-                    .HasForeignKey(d => d.PerfilId);
-            });
 
             OnModelCreatingPartial(modelBuilder);
         }
